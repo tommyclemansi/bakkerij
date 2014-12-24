71016f96f1300eab2b8cf2c8f6894860b3b08285
tcleyman <tcleyman@TCLEYMAN-LAP.be.oracle.com>
2014-12-24 13:59:04 +0100

    added a jsf page

---
+++ src/main/java/cleymans/tom/TestClass.java
@@ -0,0 +1,5 @@
+package cleymans.tom;
+
+public class TestClass {
+
+}
---
+++ src/main/java/mybeans/mainPage.java
@@ -0,0 +1,51 @@
+/**
+ * 
+ */
+package mybeans;
+
+/**
+ * @author tcleyman
+ *
+ */
+/*
+ * this is a managed bean (made it SessionScoped)
+ * this for the mainPage
+ */
+public class mainPage {
+	public mainPage() {
+	}
+	
+	private String name="Tom Cleymans";
+	private int id=0;
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String pageNavigator(String to)
+	{System.out.println("pageNavigator invoked " + to);
+	 return to;
+		
+	}
+	
+}
