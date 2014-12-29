3ef4a39a2baf883dfceb4a5513751a20f9d3f104
tcleyman <tcleyman@TCLEYMAN-LAP.be.oracle.com>
2014-12-29 17:55:01 +0100

    added logging to SFSB

--- src/main/java/myStateful/OrderProcessor.java
+++ src/main/java/myStateful/OrderProcessor.java
@@ -4,10 +4,18 @@
 package myStateful;
 
 import java.util.List;
+import java.util.logging.Logger;
+import java.util.logging.Level;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 import javax.ejb.LocalBean;
+import javax.ejb.PostActivate;
+import javax.ejb.PrePassivate;
 import javax.ejb.Stateful;
 import javax.ejb.Remove;
+
+import org.apache.tools.ant.types.LogLevel;
 
 import myentities.Taart;
 
@@ -21,6 +29,7 @@ import myentities.Taart;
 @LocalBean
 public class OrderProcessor {
 	
+	private Logger logger = Logger.getLogger(this.getClass().getName());
 	private List<Taart> orderedTaartjes;
 
     /**
@@ -28,6 +37,22 @@ public class OrderProcessor {
      */
     public OrderProcessor() {
         // TODO Auto-generated constructor stub
+    }
+    
+    
+    @PostConstruct()
+    @PostActivate()
+    public void initialize()
+    {
+    logger.log(Level.INFO,"initialize called (PostConstruct,PostActivate)");	
+    }
+    
+    
+    @PreDestroy()
+    @PrePassivate()
+    public void cleanup()
+    {
+    logger.log(Level.INFO, "");	
     }
     
     
