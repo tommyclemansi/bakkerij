b7679271c48645d54ae0264e445ec8d9b55d6bcb
tcleyman <tcleyman@TCLEYMAN-LAP.be.oracle.com>
2015-01-05 15:40:53 +0100

    added Junit test

---
+++ src/test/java/bakkerij/SoortServiceTest1.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package bakkerij;
+
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author tcleyman
+ *
+ */
+public class SoortServiceTest1 {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link myStateless.SoortService#getSoort(int)}.
+	 */
+	@Test
+	public final void testGetSoort() {
+		System.out.println("testing testGetSoort");
+		assertTrue(true);
+		System.out.println("finished testing testGetSoort"); // TODO
+	}
+
+}
