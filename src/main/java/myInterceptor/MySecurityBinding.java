ef92d86b474922a2794f63a8adbd473056c64f65
tcleyman <tcleyman@TCLEYMAN-LAP.be.oracle.com>
2015-01-06 20:00:21 +0100

    fix WELD-000069 An interceptor must have at least one binding

---
+++ src/main/java/myInterceptor/MySecurityBinding.java
@@ -0,0 +1,26 @@
+/*******************************************************************************
+ * Copyright (c) 2015 Tom Cleymans
+ *******************************************************************************/
+package myInterceptor;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import javax.interceptor.InterceptorBinding;
+/*
+ * created this because I couldn't get EJB interceptors to work
+ * so using CDI interceptors
+ * 
+ * This is InterceptorBinding
+ * now one can annotate the class or method with TomsLog to facilitate  
+ */
+@InterceptorBinding
+@Target({TYPE,METHOD})
+@Retention(RUNTIME)
+public @interface MySecurityBinding {
+
+}
